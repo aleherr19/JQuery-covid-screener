e4e889b3a2cdd7604413d0a16386b4d86253d03c
Alex Herrera <pivothdv@gmail.com>
2022-01-19 09:01:06 -0600

    Refactored code for redability

--- js/script.js
+++ js/script.js
@@ -17,24 +17,20 @@ limitations under the License.
 */
 
 
-// This holds the results of the survey questions
-// The index represents the question asked.
-// e.g. First question answered as no would be
-// index 0 with a boolean of false.
+// Holds each question as a boolean result.
+// Used for evaluation at the end process.
 var resultset = [];
 
-// A JQuery nodelist of all the question.section questions
-var $questionSections;
-
-
-
-
-
-// Document is ready to be manipulated
 $(document).ready(function() {
 
-  // Play the landing page animation
-  landingAnimation();
+  // Animate questions in. Landing animation.
+  $(".question").each(function(index) {
+    var _this = $(this);
+
+    _this.css({opacity: '0'});
+    _this.animate({opacity: '1' }, 100 + (index * 400));
+  });
+
 
   // Get a list of all the YES and NO buttons
   var $buttonListNegative = $(".negative");
@@ -43,58 +39,25 @@ $(document).ready(function() {
   // When No is clicked on a question
   $buttonListNegative.each(function(index) {
     $(this).click(function() {
-      buttonClick($(this).parent(), index, false);
+      // Animate question block to hide
+      $(this).parent().slideUp(400, "easeOutExpo", null);
+
+      addResultToSet(index, false);
     });
   });
 
   // When Yes is clicked on a question
   $buttonListPositive.each(function(index) {
     $(this).click(function() {
-      buttonClick($(this).parent(), index, true);
+      // Animate question block to hide
+      $(this).parent().slideUp(400, "easeOutExpo", null);
+      
+
+      addResultToSet(index, true);
     });
   });
 
 });
-
-
-
-// Animation for when the user first loads on this page
-function landingAnimation() {
-  // Get a nodelist of all the question elements
-  var $questionSections = $("section.question");
-
-  // Animate the first few question blocks. 
-  // The rest does not need it as it's outisde
-  // the visible area to the user.
-  $questionSections.each(function(index) {
-    // Context for this instance of a section.question element
-    var that = $(this);
-    
-    // When the page is first loaded, a fade-in animation will
-    // play. We don't need to animate all of them (off screen ones)
-    // Just the first few on screen
-    if (index < ($questionSections.length / 3)) {
-      // Make the element invisible
-      that.css({opacity: '0'});
-
-      // Fade in the element
-      that.animate({opacity: '1' }, 100 + (index * 400));
-      
-    }
-  });
-}
-
-
-
-
-function buttonClick($elementToAnimate, index, isYes) {
-// Hide this element in a fade animation
-    $elementToAnimate.slideUp(400, "easeOutExpo", null);
-
-    // Put false result into the set
-    addResultToSet(index, isYes);
-}
-
 
 
 
@@ -109,41 +72,29 @@ function addResultToSet(index, boolean) {
 }
 
 
-
-
-// Evaluate the questions & display a prompt of
-// the next action the user should take
+// Evaluate the questions 
 function evaluateQuestions() {
-  // Grab the default "note/disclaimer" notification
-  var $notif = $(".notification");
-
-  // Evaluate the survey. If it doesn't
-  // match this pattern (Best of my knowledge,
-  // its correct) modify the "note/disclaimer"
-  // notification accordingly
-  //
+  // Evaluate the questions if the
+  // user may be at risk or not.
   if (resultset[0]==true ||
-      resultset[2]==true ||
-      resultset[3]==true ||
-      resultset[4]==true ||
-      resultset[5]==true ||
-      resultset[7]==true ||
-      resultset[8]==true ||
-      resultset[9]==true) {
+        resultset[2]==true ||
+          resultset[3]==true ||
+            resultset[4]==true ||
+              resultset[5]==true ||
+                resultset[7]==true ||
+                  resultset[8]==true ||
+                    resultset[9]==true) {
 
       // At Risk!
       $("header > h2").text("At Risk.");
       $("header > p").text("Based on the survey, you don't sound well. We're not telling you what to do, but you should stay home.");
       $("header").css("background-color", "#ffd9cc");
-
-
     } else {
-
       // Good to go
       $("header > h2").text("You're safe.");
       $("header > p").text("Based on the survey, you sound well. We're not telling you what to do, but you're good to go if you need to.");
       $("header").css("background-color", "#dcffcc");
-
   }
+
 
 }
